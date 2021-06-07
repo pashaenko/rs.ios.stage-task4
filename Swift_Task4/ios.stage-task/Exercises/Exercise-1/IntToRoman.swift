dfd80f4049a29e8b462a7480dcdf129514a15d0f
pashaenko <pashaenko@icloud.com>
2021-06-07 21:19:22 +0300

    pushTask

--- Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
@@ -3,6 +3,31 @@ import Foundation
 public extension Int {
     
     var roman: String? {
-        nil
+        if (self < 1 || self > 3999) { return nil }
+        
+        var pointValue = self
+        var numerationString = ""
+        let romanList = [(1000, "M"),
+                           (900, "CM"),
+                           (500, "D"),
+                           (400, "CD"),
+                           (100, "C"),
+                           (90, "XC"),
+                           (50, "L"),
+                           (40, "XL"),
+                           (10, "X"),
+                           (9, "IX"),
+                           (5, "V"),
+                           (4, "IV"),
+                           (1, "I")]
+        
+        for value in romanList {
+            while pointValue >= value.0 {
+                pointValue -= value.0
+                numerationString += value.1
+            }
+        }
+        
+        return numerationString
     }
 }
